04f9e54bf2dd17d779d060e70d532d9a83caa8a5
kay6888 <kaynikko88@gmail.com>
2025-07-26 08:36:02 -0500

    Update derp_fogo.mk

--- derp_fogo.mk
+++ derp_fogo.mk
@@ -11,8 +11,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from fogos device
 $(call inherit-product, device/motorola/fogo/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Derpfest stuff.
+$(call inherit-product, vendor/derpfest/config/common_full_phone.mk)
 
 PRODUCT_NAME := derp_fogo
 PRODUCT_DEVICE := fogo
