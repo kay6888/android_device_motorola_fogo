5520857185e81d65c04ebb3e91ba693260fe4452
Michael Bestas <mkbestas@lineageos.org>
2025-07-28 05:50:27 +0300

    fogo: Update to U1UFNS34.41-98-3-19
    Change-Id: Idf4d47c10a5a50f54f0b846c317332b58302ef2d

--- lineage_fogo.mk
+++ lineage_fogo.mk
@@ -23,6 +23,6 @@ PRODUCT_MODEL := moto g 5G - 2024
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="fogo_g-user 14 U1UFNS34.41-98-3-16 31deb6-790f4 release-keys MUR1-0.252" \
-    BuildFingerprint=motorola/fogo_g/fogo:14/U1UFNS34.41-98-3-16/31deb6-790f4:user/release-keys \
+    BuildDesc="fogo_g-user 14 U1UFNS34.41-98-3-19 7ab7a1-86d9da release-keys MUR1-0.252" \
+    BuildFingerprint=motorola/fogo_g/fogo:14/U1UFNS34.41-98-3-19/7ab7a1-86d9da:user/release-keys \
     DeviceProduct=fogo_g
